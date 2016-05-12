047418a99a29a888530757117f11824000f85891
LukeSkypewalker <work37@gmail.com>
2016-05-12 15:30:40 +0300

    lab 11

---
+++ functions/calcTrajectory11.m
@@ -0,0 +1,15 @@
+function [ X ] = calcTrajectory11( Acc, x1, v1, t)
+    
+    n=length(Acc);
+    X(n) = zeros();
+    X(1) = x1;
+    Vel(n) = zeros();
+    Vel(1) = v1;
+
+    for i = 2:n
+        Vel(i) = Vel(i-1) + Acc(i-1)*t;
+        X(i) = X(i-1) + Vel(i-1)*t + (Acc(i-1)*t^2)/2;
+    end
+
+end
+
