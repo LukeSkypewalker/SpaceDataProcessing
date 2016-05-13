98130e57889ee3c1a8b8cf47ba96d20596e92a79
LukeSkypewalker <work37@gmail.com>
2016-05-13 14:50:41 +0300

    lab12

--- functions/calcKalmanExtended.m
+++ functions/calcKalmanExtended.m
@@ -7,16 +7,16 @@ function [ Xk ] = calcKalmanExtended(Z, Z0, F, P, Q, R )
     for i=2:n
         
         Xk(:,i) = F*Xk(:, i-1);
-        P1 = F*P*F'+Q;
+        P = F*P*F'+Q;
         
         sqr = sqrt(Xk(1,i)^2+Xk(3,i)^2) ;
         H = [ sqr ; atan(Xk(1,i)/Xk(3,i)) ]; 
         dH = [  Xk(1,i)/sqr  0 Xk(3,i)/sqr  0; 
                 Xk(3,i)/(sqr^2) 0 -Xk(1,i)/(sqr^2)  0 ];
         
-        K = P1 * dH' * inv(dH * P * dH' + R);   
+        K = P * dH' * inv(dH * P * dH' + R);   
         Xk(:,i) = Xk(:,i) + K*(Z(:,i) - H);
-        P = (eye(4)-K*dH) * P1;
+        P = (eye(4)-K*dH) * P;
       
     end
 
