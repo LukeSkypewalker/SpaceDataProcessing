aac5a176651d252b88c0be5bb38ee7645f9fff32
LukeSkypewalker <work37@gmail.com>
2016-05-13 15:38:38 +0300

    lab 12 end of lesson

--- functions/calcKalmanExtended12.m
+++ functions/calcKalmanExtended12.m
@@ -9,10 +9,18 @@ function [ Xk ] = calcKalmanExtended12(Z, Z0, F, P, Q, R )
         Xk(:,i) = F*Xk(:, i-1);
         P = F*P*F'+Q;
         
-        sqr = sqrt(Xk(1,i)^2+Xk(3,i)^2) ;
-        H = [ sqr ; atan(Xk(1,i)/Xk(3,i)) ]; 
-        dH = [  Xk(1,i)/sqr  0 Xk(3,i)/sqr  0; 
-                Xk(3,i)/(sqr^2) 0 -Xk(1,i)/(sqr^2)  0 ];
+            
+        if mod(i,2)==0
+            sq = (Xk(1,i)^2+Xk(3,i)^2) ;
+            H = atan(Xk(1,i)/Xk(3,i)) ; 
+            dH = [ Xk(3,i)/sq 0 -Xk(1,i)/sq 0 ];
+        else
+            sqr = sqrt(Xk(1,i)^2+Xk(3,i)^2) ;
+            H = [ sqr ; atan(Xk(1,i)/Xk(3,i)) ];
+            dH = [  Xk(1,i)/sqr  0 Xk(3,i)/sqr  0; 
+                    Xk(3,i)/(sqr^2) 0 -Xk(1,i)/(sqr^2)  0 ];
+        
+        end
         
         K = P * dH' * inv(dH * P * dH' + R);   
         Xk(:,i) = Xk(:,i) + K*(Z(:,i) - H);
